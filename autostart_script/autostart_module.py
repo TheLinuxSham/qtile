73f97587c483d161d56a93abfb060c9a4b75b177
Jan Schneider <j.schn@tutanota.com>
2024-01-11 19:17:03 +0100

    fixed typo

--- autostart_script/autostart_module.py
+++ autostart_script/autostart_module.py
@@ -4,7 +4,7 @@ import subprocess
 
 
 @hook.subscribe.startup_once
-def autostart_once(script_path: id):
+def autostart_once(script_path: str):
     home = os.path.expanduser(script_path)
     subprocess.call([home])
 
